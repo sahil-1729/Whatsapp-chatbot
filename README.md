42936198260709f58de18601080146ef4cc80664
sahil-1729 <sahil.maji@gmail.com>
2023-06-19 14:52:04 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Chatbot Using Bard
+### This project is an implementation of a chatbot using the Bard framework and Node.js. The chatbot is designed to interact with users through a terminal interface, providing an intuitive and conversational experience.
+# Installation
+
+    Clone the project repository from GitHub: git clone https://github.com/your-username/your-repo.git
+    Navigate to the project directory: cd your-repo
+    Install the required dependencies: npm install
+
+# Usage
+
+    Open a terminal or command prompt.
+    Navigate to the project directory: cd your-repo
+    Start the chatbot: node app.js
+    Interact with the chatbot through the terminal interface.
---
+++ index.js
@@ -0,0 +1,25 @@
+// require('dotenv').config()
+//Since using modules(used type : module in package.json), that's why used import instead of require
+import 'dotenv/config'
+import readline from 'readline'
+import Bard from 'bard-ai'
+//insert the token from f12-> application-> cookies-> __Secure-1PSID
+await Bard.init(process.env.TOKEN);
+// to continue the conversation, not just take only 1 prompt
+let myChat = new Bard.Chat();
+let result
+const UI = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+UI.prompt("Enter the prompt")
+UI.on("line",async(input) => {
+    //If input entered is end, stop the bot
+    if(input === "end"){
+        UI.close()
+    }
+    result = input
+    console.log(await myChat.ask(result))
+    console.log(`Enter "end" to end the conversation`)
+})
+
